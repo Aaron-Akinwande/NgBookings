aefa5a2d951f6931250759c09394f4dd16de739a
Aaron <aaronakinwande@gmail.com>
2025-07-02 14:41:45 +0100

    dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -12,12 +12,12 @@ export default function page() {
         <div className="flex items-center justify-between mb-3 p-4 bg-white">
           <h1 className="text-2xl font-bold text-gray-900">Overview</h1>
           <div className="flex items-center space-x-4">
-            <div className="relative">
+            <div className="relative hidden md:block">
               <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-4 h-4" />
               <input
                 type="text"
                 placeholder="Search"
-                className="pl-10 pr-4 py-2 border border-gray-300 rounded-lg "
+                className="pl-10 pr-4 py-2 border border-gray-300 rounded-lg"
               />
             </div>
             <button className="bg-[#7E0140] flex gap-1 text-white px-4 py-2 rounded-lg">
